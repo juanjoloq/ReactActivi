d6862277a3c3228921a5339062faccbe9e35d8c6
Juan Jose Duque Alvarez <juandual2607@gmail.com>
2025-05-21 06:47:40 -0500

    Update NoticiaDestacada.jsx

--- src/components/NoticiaDestacada.jsx
+++ src/components/NoticiaDestacada.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import PaginaLayout from "./PaginaLayout";
 
-function NoticiaDestacada({ titulo, contenido, fecha }) {
+function NoticiaDestacada({ titulo, contenido }) {
+  const ahora = new Date().toLocaleTimeString('es-ES', {
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric'
+})
   return (
     <PaginaLayout>
       <div className="d-flex justify-content-center align-items-center" >
         <article className="card my-4 shadow-sm text-center w-100" style={{ maxWidth: "600px" }}>
           <header className="card-header bg-dark text-white">
             <h2 className="card-title h5 mb-1">{titulo}</h2>
-            <small>{fecha}</small>
+            <small>{ahora}</small>
           </header>
           <div className="card-body">
             <p className="card-text">{contenido}</p>
